cc8c314684e74c177b6ce79e18a36ac475cdae90
Paul-Gabriel <gabriel_paul11@yahoo.com>
2025-03-05 13:21:51 +0200

    Update Firebase credentials and add .gitignore for sensitive files

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import os
 
-cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), 'test-44edc-firebase-adminsdk-cakyn-5cf778f7e8.json'))
+cred = credentials.Certificate(os.path.join(os.path.dirname(__file__), 'test-44edc-firebase-adminsdk-cakyn-75aec87cb6.json'))
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
